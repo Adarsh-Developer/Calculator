00374100dd508ecb5e1ddc2553f740297bd8005a
Adarsh <adarshofficial.1607@gmail.com>
2023-12-09 22:44:32 +0530

    adding operation

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
-const operators = document.querySelectorAll('.num')
-const screen = document.querySelector('.screen')
-const equal = document.querySelector('.equal')
-const clear = document.querySelector('.clear')
-const cross = document.querySelector('.cross')
-const audio = new Audio('./audio.mp3')
+const operators = document.querySelectorAll(".num");
+const screen = document.querySelector(".screen");
+const equal = document.querySelector(".equal");
+const clear = document.querySelector(".clear");
+const cross = document.querySelector(".cross");
+const audio = new Audio("./audio.mp3");
 
-operators.forEach(function(e){
-  e.addEventListener('click', function(){
-    screen.innerHTML += e.getAttribute('data')
-  })
-})
+let operation = "";
 
-equal.addEventListener('click', function(){
-  if(screen.innerHTML !== ''){
-    screen.innerHTML = eval(screen.innerHTML)
+operators.forEach(function (e) {
+  e.addEventListener("click", function () {
+    operation += e.getAttribute("data");
+    screen.innerHTML += e.innerHTML;
+  });
+});
+
+equal.addEventListener("click", function () {
+  if (screen.innerHTML !== "") {
+    screen.innerHTML = eval(operation);
   }
-})
+});
 
-clear.addEventListener('click', function(){
-  screen.innerHTML = ''
-})
+clear.addEventListener("click", function () {
+  screen.innerHTML = "";
+  operation = "";
+});
 
-cross.addEventListener('click', function(){
-  screen.innerHTML = screen.innerHTML.slice(0, -1)
-})
+cross.addEventListener("click", function () {
+  screen.innerHTML = screen.innerHTML.slice(0, -1);
+  operation = operation.slice(0, -1);
+});
