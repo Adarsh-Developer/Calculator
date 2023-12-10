4d80fcfe74ee9d49bf302c640ca1831099ad13e8
Adarsh <adarshofficial.1607@gmail.com>
2023-12-10 12:27:06 +0530

    error fixing

--- script.js
+++ script.js
@@ -4,28 +4,23 @@ const equal = document.querySelector(".equal");
 const clear = document.querySelector(".clear");
 const cross = document.querySelector(".cross");
 
-let operation = "";
 
 operators.forEach(function (e) {
   e.addEventListener("click", function () {
-    operation += e.getAttribute("data");
     screen.innerHTML += e.innerHTML;
   });
 });
 
 equal.addEventListener("click", function () {
   if (screen.innerHTML !== "") {
-    screen.innerHTML = eval(operation)
-    operation = eval(operation)
+    screen.innerHTML = eval(screen.innerHTML);
   }
 });
 
 clear.addEventListener("click", function () {
   screen.innerHTML = "";
-  operation = "";
 });
 
 cross.addEventListener("click", function () {
-  screen.innerHTML = screen.innerHTML.slice(0, -5);
-  operation = operation.slice(0, -1);
+  screen.innerHTML = screen.innerHTML.slice(0, -1);
 });
